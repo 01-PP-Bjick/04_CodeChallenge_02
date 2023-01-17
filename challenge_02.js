98140b87502b0341c57aaea69b39e589d5fce45e
bjick84 <bjick@web.de>
2023-01-17 09:30:01 +0100

    name mit user getauscht

--- challenge_02.js
+++ challenge_02.js
@@ -1,7 +1,7 @@
 /*********   CODE CHALLENGE 02 **********/
 
 /*
-Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung einen usern ein
 Geben über eine Prompt-Anforderung ein Alter ein
 
 Geben Sie in Abhängigkeit vom angegebenen Alter 
@@ -16,7 +16,7 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
-var name = prompt("Please enter your name:");
+var user = prompt("Please enter your user:");
 var age = prompt("Please enter your Age:");
 
 // age = ">= 0 && <=5";
@@ -24,39 +24,39 @@ var age = prompt("Please enter your Age:");
 // age = ">= 13 && age <= 17";
 // age = ">= 18";
 
-// switch (true) 
+switch (true) 
+{
+    case (age >= 0 && age <= 5):
+        console.log(user + ", you should have some milk.");
+        break;
+    case (age >= 6 && age <= 12):
+        console.log(user + ", you should have some juice.");
+        break;
+    case (age >= 13 && age <= 17):
+        console.log(user + ", you should have some cola.");
+        break;
+    case (age >= 18):
+        console.log(user + ", you should have some wine.");
+        break;
+
+    default:
+        console.log("Invalid age input.");
+        break;
+}
+
+// if (age >= 0 && age <= 5)
 // {
-//     case (age >= 0 && age <= 5):
-//         console.log(name + ", you should have some milk.");
-//         break;
-//     case (age >= 6 && age <= 12):
-//         console.log(name + ", you should have some juice.");
-//         break;
-//     case (age >= 13 && age <= 17):
-//         console.log(name + ", you should have some cola.");
-//         break;
-//     case (age >= 18):
-//         console.log(name + ", you should have some wine.");
-//         break;
-
-//     default:
-//         console.log("Invalid age input.");
-//         break;
+//     console.log(user + ", you should have some milk.");
 // }
-
-if (age >= 0 && age <= 5)
-{
-    console.log(name + ", you should have some milk.");
-}
-else if (age >= 6 && age <= 12)
-{
-    console.log(name + ", you should have some juice.");
-}
-else if (age >= 13 && age <= 17)
-{
-    console.log(name + ", you should have some cola.");
-}
-else (age >= 18)
-{
-    console.log(name + ", you should have some wine.");
-}
+// else if (age >= 6 && age <= 12)
+// {
+//     console.log(user + ", you should have some juice.");
+// }
+// else if (age >= 13 && age <= 17)
+// {
+//     console.log(user + ", you should have some cola.");
+// }
+// else (age >= 18)
+// {
+//     console.log(user + ", you should have some wine.");
+// }
